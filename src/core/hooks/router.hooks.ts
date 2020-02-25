cb2f3249dd0dd003f497a934303bec8bc6e5432d
编码猿 <www.2271608011@qq.com>
2020-02-25 21:00:06 +0800

    不提供src源码！！

--- src/core/hooks/router.hooks.ts
+++
@@ -1,8 +0,0 @@
-import { computed } from '@vue/composition-api';
-import { getRuntimeVM } from '../utils/runtime.utils';
-
-export function useRouter() {
-  const vm = getRuntimeVM();
-  const route = computed(() => vm.$route);
-  return { route, router: vm.$router };
-}
